68f8796c99d40748b69f586ea569e612963bdcfd
nailarizki <hello.nailarizki@gmail.com>
2026-02-22 23:14:28 +0700

    implementasi adt graph adjacency matrix

---
+++ praktikum_graph/adjacencymatrix/graph_matrix.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "graph_matrix.h"
+
+// create graph init
+void initGraph(GraphMatrix *g) {
+    g->numVertices = 0;
+    for (int i = 0; i < MAX_VERTEX; i++) {
+        for (int j = 0; j < MAX_VERTEX; j++) {
+            g->edges[i][j] = 0; // 0 menandakan tidak ada edge
+        }
+    }
+}
+
+    // add vertex 
+    void addVertex(GraphMatrix *g, char name) {
+        if (g->numVertices < MAX_VERTEX) {
+            g->vertices[g->numVertices] = name;
+            g->numVertices++;
+        } else {
+            printf("Graf Penuh!\n");
+        }
+    }
+
+// fungsi cari index
+int getVertexIndex(GraphMatrix *g, char name) {
+    for (int i = 0; i < g->numVertices; i++) {
+        if (g->vertices[i] == name) return i;
+    }
+    return -1;
+}
+
+// add edge
+void addEdge(GraphMatrix *g, char src, char dest, int weight) {
+    int srcIndex = getVertexIndex(g, src);
+    int destIndex = getVertexIndex(g, dest);
+
+    if (srcIndex != -1 && destIndex != -1) {
+        // graph directed
+        g->edges[srcIndex][destIndex] = weight;
+    } else {
+        printf("Vertex tidak ditemukan!\n");
+    }
+}
+
+// print graph
+void printGraph(GraphMatrix g) {
+    printf("--- ADJACENCY MATRIX ---\n  ");
+    for (int i = 0; i < g.numVertices; i++) {
+        printf("%c ", g.vertices[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < g.numVertices; i++) {
+        printf("%c ", g.vertices[i]);
+        for (int j = 0; j < g.numVertices; j++) {
+            printf("%d ", g.edges[i][j]);
+        }
+        printf("\n");
+    }
+}
