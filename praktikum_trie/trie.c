43443fe11b0daef593545c6aacaca25e08df0950
Refina Aprilia Dewi <refinaapriliad@gmail.com>
2026-03-14 23:47:41 +0700

    Add search, startswith

--- praktikum_trie/trie.c
+++ praktikum_trie/trie.c
@@ -25,3 +25,35 @@ void insert(struct TrieNode* root, const char* key){
     }
     current->isEndOfWord = 1; //batas akhir nama kontak
 }
+
+int search(struct TrieNode* root, const char* key){
+    struct TrieNode* current = root;
+
+    for (int i = 0; key[i] != '\0'; i++){
+        int index = key[i] - 'a';
+
+        if (current->children[index] == NULL){
+            return 0;
+        }
+
+        current = current->children[index];
+    }
+
+    return (current != NULL && current->isEndOfWord);
+}
+
+int startsWith(struct TrieNode* root, const char* prefix){
+    struct TrieNode* current = root;
+
+    for (int i = 0; prefix[i] != '\0'; i++){
+        int index = prefix[i] - 'a';
+
+        if (current->children[index] == NULL){
+            return 0;
+        }
+
+        current = current->children[index];
+    }
+
+    return 1;
+}
